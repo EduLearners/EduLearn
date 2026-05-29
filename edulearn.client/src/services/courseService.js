f54508a5997bae08c7dd9cd38bc0c5a19128d252
TanyaShakyaa <2487465@cognizant.com>
2026-05-29 15:12:18 +0530

    chore(client): remove dead courseService methods that 404

--- edulearn.client/src/services/courseService.js
+++ edulearn.client/src/services/courseService.js
@@ -31,34 +31,4 @@ export const courseService = {
         const { data } = await axiosClient.delete(`/courses/${id}`);
         return data;
     },
-
-    // GET /api/courses/:id/syllabus
-    getSyllabus: async (id) => {
-        const { data } = await axiosClient.get(`/courses/${id}/syllabus`);
-        return data;
-    },
-
-    // PUT /api/courses/:id/syllabus
-    updateSyllabus: async (id, syllabusData) => {
-        const { data } = await axiosClient.put(`/courses/${id}/syllabus`, syllabusData);
-        return data;
-    },
-
-    // GET /api/courses/:id/prerequisites
-    getPrerequisites: async (id) => {
-        const { data } = await axiosClient.get(`/courses/${id}/prerequisites`);
-        return data;
-    },
-
-    // POST /api/courses/:id/prerequisites
-    addPrerequisite: async (id, prerequisiteData) => {
-        const { data } = await axiosClient.post(`/courses/${id}/prerequisites`, prerequisiteData);
-        return data;
-    },
-
-    // DELETE /api/courses/:id/prerequisites/:prereqId
-    removePrerequisite: async (id, prereqId) => {
-        const { data } = await axiosClient.delete(`/courses/${id}/prerequisites/${prereqId}`);
-        return data;
-    },
 };
